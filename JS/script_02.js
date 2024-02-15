010aea1f70c76de30ccb12b2ed27e2cff04335ca
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-02-15 11:32:11 -0600

    Agregué funciones para que las tarjetas sean variadas

--- JS/script_02.js
+++ JS/script_02.js
@@ -1,46 +1,63 @@
-var grupoTarjetas1 = ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺",];
-var grupoTarjetas2 = ["😐", "🤓", "🧐", "🥰", "🤑", "😈", "💀", "🤢",];
-var grupoTarjetas3 = ["🍔", "🍟", "🍕", "🌭", "🥪", "🌮", "🌯", "🥙",];
+var Tarjetas1 = ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺",];
+// var grupoTarjetas2 = ["😐", "🤓", "🧐", "🥰", "🤑", "😈", "💀", "🤢",];
+// var grupoTarjetas3 = ["🍔", "🍟", "🍕", "🌭", "🥪", "🌮", "🌯", "🥙",];
 
-var totaltarjetas1 = grupoTarjetas1 + grupoTarjetas2 + grupoTarjetas3;
+// var totaltarjetas1 = grupoTarjetas1 + grupoTarjetas2 + grupoTarjetas3;
 
-var totaltarjetas2 = grupoTarjetas1.concat(grupoTarjetas2, grupoTarjetas3);
+// var totaltarjetas2 = grupoTarjetas1.concat(grupoTarjetas2, grupoTarjetas3);
 
-var totaltarjetas3 = grupoTarjetas1.concat(grupoTarjetas1);
+var totalTarjetas = Tarjetas1.concat(Tarjetas1);
 
-var arrayanadidado = [
-    ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺",],
-    ["😐", "🤓", "🧐", "🥰", "🤑", "😈", "💀", "🤢",],
-    ["🍔", "🍟", "🍕", "🌭", "🥪", "🌮", "🌯", "🥙",]
-];
+function barajaTarjetas() {
+    var resultado;
+    resultado = totalTarjetas.sort(function () {
+        return 0.5 - Math.random();
+    });
+    return resultado;
+}
+
+
+// var arrayanadidado = [
+//     ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺",],
+//     ["😐", "🤓", "🧐", "🥰", "🤑", "😈", "💀", "🤢",],
+//     ["🍔", "🍟", "🍕", "🌭", "🥪", "🌮", "🌯", "🥙",]
+// ];
 
 
 
 function repartetarjetas(){
+    var mesa = document.getElementById("mesa");
+    var tarjetasBarajadas = barajaTarjetas();
 
-    var nesa = document.getElementById("#mesa");
+    console.clear();
 
-    console.innerHTML = "";
-
-
-    totaltarjetas3.forEach(function (elemento) {
+    tarjetasBarajadas.forEach(function (elemento) {
             var tarjeta = document.createElement("div");
 
             tarjeta.innerHTML =
-            "<div class='card text-center mb-3' >"+
-            "<div class='card-body'>"+
+            "<div class='tarjeta'>"+
+            "<div class='tarjeta__contenido'>"+
               elemento +
             "</div>"+   
             "</div>";  
 
             mesa.appendChild(tarjeta);  
-        
         }
     );
 }
 
+function descubrir() {
+    this.classList.add("descubierta");
+}
+
 repartetarjetas();
 
-for (i = 0; i <) 5; i++) {
-    console.log(i);
-}
+document.querySelectorAll(".tarjeta").forEach(
+    function (elemento) {
+        elemento.addEventListener("click", descubrir);
+    }
+);
+
+
+
+console.log(barajaTarjetas());
